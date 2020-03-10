9a6ea4faedd6f17bc88bd028c592129a45c5b766
Carlos Eduardo <caiean0308@gmail.com>
2020-03-10 20:42:10 -0300

    Finalização do exercicio da aula 2

---
+++ Aula2/src/factorymethod/Nome.java
@@ -0,0 +1,24 @@
+package factorymethod;
+
+public class Nome {
+	private String nome, sobrenome;
+
+	public Nome(String nome, String sobrenome) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	@Override
+	public String toString() {
+		return nome + " " + sobrenome;
+	}	
+}
+
